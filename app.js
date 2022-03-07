a27495c10f80e65d577e1e4d03e9b018de15a70e
rterrado <terrado.ken@gmail.com>
2022-03-07 16:02:04 +0800

    adding push endpoint

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require ('express')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
+var cors = require('cors')
+const fileupload = require('express-fileupload')
+const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 5050
 
 // get config vars
 dotenv.config()
+
+app.use(cors(),fileupload());
 
 app.get('/rotate',function(req,res){
     if (req.query.token===''||req.query.token===undefined) {
@@ -40,6 +45,23 @@ app.get('/rotate',function(req,res){
             })
         }
     })
+})
+
+app.post('/push',function(req,res){
+
+    const fileName = req.files.myFile.name
+    const path = __dirname + '/tmp/images/' + fileName
+
+    req.files.myFile.mv(path, (error) => {
+        if (error) {
+          res.status(500)
+          res.json({error:'internal server error'})
+          return
+        }
+        res.status(200)
+        res.json({success:'image uploaded'})
+    })
+
 })
 
 app.use(function(req, res){
